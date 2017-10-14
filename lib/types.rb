a1fc663ee76ef827336724558baf2feb50d52abc
HanisahKZ <hanisahkz@gmail.com>
2017-10-14 12:48:32 +0800

    test passes

--- lib/types.rb
+++ lib/types.rb
@@ -1,6 +1,10 @@
+#this is a very basic dry setup
 require "dry-struct"
 require "dry-types"
 
 module Types
   include Dry::Types.module
+
+  #exe begin here
+  ArticleStatus = Strict::String.default("draft").enum("draft", "published")
 end
